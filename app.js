1aa68a610ac04f0628f2776cb3bdf9d7dd3f74a9
Nourelhouda Taroudi <nourelhoudataroudi@gmail.com>
2022-06-26 16:55:52 +0100

    Nour : Added entry page

---
+++ app.js
@@ -0,0 +1,6 @@
+function showChoices(){
+    let btn = document.getElementById('show-btn');
+    let carsoul = document.getElementById('carsoul');
+    carsoul.style.display="flex";
+    btn.style.display="none"
+  }
